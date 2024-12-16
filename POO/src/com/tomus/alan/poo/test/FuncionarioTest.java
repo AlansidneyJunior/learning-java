eb8c059e9e6420232c36302d345b423f5652a6ab
AlansidneyJunior <alansidney.silva21@gmail.com>
2024-12-16 10:30:16 -0300

    sync

---
+++ POO/src/com/tomus/alan/poo/test/FuncionarioTest.java
@@ -0,0 +1,13 @@
+package com.tomus.alan.poo.test;
+
+import com.tomus.alan.poo.classes.Funcionario;
+
+public class FuncionarioTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Funcionario func1 = new Funcionario("Alan","000111222-44",2300.00);
+		func1.imprime();
+	}
+
+}
