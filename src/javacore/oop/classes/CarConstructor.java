beb4f9addef5028ece03b2e5087eb8d090e50b67
AlansidneyJunior <alansidney.silva21@gmail.com>
2025-05-07 20:33:57 -0300

    Fundamento da OOP (encapsulamento, construtores e sobrecarga)

---
+++ src/javacore/oop/classes/CarConstructor.java
@@ -0,0 +1,44 @@
+package javacore.oop.classes;
+
+public class CarConstructor {
+	private String name;
+	private String brand;
+	private int year;
+
+	public CarConstructor(String name, String brand, int year) {
+		super();
+		this.name = name;
+		this.brand = brand;
+		this.year = year;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [name=" + name + ", brand=" + brand + ", year=" + year + "]";
+	}
+
+}
---
+++ src/javacore/oop/test/CarConstructorTest.java
@@ -0,0 +1,13 @@
+package javacore.oop.test;
+
+import javacore.oop.classes.CarConstructor;
+
+public class CarConstructorTest {
+
+	public static void main(String[] args) {
+		CarConstructor car = new CarConstructor("Mobi", "Fiat", 2018);
+
+		System.out.println(car);
+	}
+
+}
